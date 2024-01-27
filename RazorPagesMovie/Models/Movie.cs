e0f7c039d1966b7957090711bbc6a95b765bd708
Iykestine <roboticike@yahoo.com>
2024-01-27 17:45:49 +0100

    First commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        //With this attribute 1. User isn't required to enter TIME INFORMATION in the date field. 2. Only the date is displayed, NOT TIME Information 
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
